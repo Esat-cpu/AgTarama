3fe1e84377daa609d05e1377ab24e00c062833a3
Esat-cpu <saygina7@gmail.com>
2025-04-23 13:23:39 +0300

    Geçersiz arguman verilme durumunda uyarı mesajı eklendi.

--- emap.sh
+++ emap.sh
@@ -6,9 +6,14 @@ BASE="${IP%.*}"
 for i in $(seq 0 0xff); do
     TARGET="$BASE.$i"
 
-    if [[ "$1" =~ '^[0-9]$|^[0-9]?\.[0-9]+$' ]]; then SN="$1"
-    else SN=0.5; fi
+    if [[ "$1" =~ '^[0-9]$|^[0-9]?\.[0-9]+$' ]]
+        then SN="$1"
+    else
+        echo "Geçersiz arguman!" >&2
+        SN=0.5
+    fi
 
+    
     if ping -c 1 -W $SN $TARGET &> /dev/null; then
         echo "$TARGET Host is up!"
     fi
