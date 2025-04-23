b4c3e68293ee0e9bcd7091fae8913127edfaacbe
Esat-cpu <saygina7@gmail.com>
2025-04-23 15:08:18 +0300

    Flag yapısı eklendi.

--- emap.sh
+++ emap.sh
@@ -1,19 +1,50 @@
 #!/bin/bash
 
-IP=`hostname -I`
-BASE="${IP%.*}"
+SN=0.5
 
-if [[ "$1" =~ '^[0-9]$|^[0-9]?\.[0-9]+$' ]]
-    then SN="$1"
-else
-    echo "Geçersiz arguman!" >&2
-    SN=0.5
-fi
+while getopts "t:h" flag; do
+    case "$flag" in
+        h)
+            echo "Kullanım: Komut [option] [argüman]"
+            echo "   -h help"
+            echo "   -t ping'lerin geri dönüşünü bekleme süresi"
+            echo "   -i taranacak IP adresini manuel olarak belirleme"
+            exit 0
+            ;;
+            
+        t)
+            if [[ "$OPTARG" =~ '^[0-9]$^[0-9]?\.[0-9]+$' ]]; then
+                SN="$OPTARG"
+            else
+                echo "Geçersiz saniye değeri girildi." >&2
+                exit 1
+            fi
+            ;;
+            
+        i) 
+            if [[ "$OPTARG" =~ '^([0-9]{1,3}\.){3}[0-9]$' ]]; then
+                IP="$OPTARG"
+            else
+                echo "Geçersiz IP adres formatı girildi." >&2
+                exit 2
+            fi
+            ;;
+
+        *)
+            echo "Geçersiz kullanım." >&2
+            exit 3
+            ;;
+    esac
+done
+
+
+IP="${IP:-$(hostname -I)}"
+BASE="${IP%.*}"
 
 for i in $(seq 0 0xff); do
     TARGET="$BASE.$i"
     
-    if ping -c 1 -W $SN $TARGET &> /dev/null; then
+    if ping -c 1 -W $SN $TARGET > /dev/null; then
         echo "$TARGET Host is up!"
     fi
 
