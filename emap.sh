cde2794b7ad113226d7955a254d36c200ad8a007
Esat-cpu <saygina7@gmail.com>
2025-04-23 14:34:19 +0300

    Argüman kontrolünün yeri döngü dışına çıkarılarak düzeltildi.

--- emap.sh
+++ emap.sh
@@ -3,16 +3,15 @@
 IP=`hostname -I`
 BASE="${IP%.*}"
 
+if [[ "$1" =~ '^[0-9]$|^[0-9]?\.[0-9]+$' ]]
+    then SN="$1"
+else
+    echo "Geçersiz arguman!" >&2
+    SN=0.5
+fi
+
 for i in $(seq 0 0xff); do
     TARGET="$BASE.$i"
-
-    if [[ "$1" =~ '^[0-9]$|^[0-9]?\.[0-9]+$' ]]
-        then SN="$1"
-    else
-        echo "Geçersiz arguman!" >&2
-        SN=0.5
-    fi
-
     
     if ping -c 1 -W $SN $TARGET &> /dev/null; then
         echo "$TARGET Host is up!"
